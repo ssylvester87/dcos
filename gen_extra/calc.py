7d36499ed58c62e74925cbab8778d9a6453e8849
Senthil Kumaran <senthil@uthcode.com>
2016-07-20 10:38:29 -0700

    Addressing Review Comment on customer_key size.

--- gen_extra/calc.py
+++ gen_extra/calc.py
@@ -4,8 +4,7 @@ from base64 import b64encode
 
 def validate_customer_key(customer_key):
     assert isinstance(customer_key, str), "'customer_key' must be a string."
-    assert len(customer_key) == 36 or len(customer_key) == 32, (
-        "'customer_key' must be 36 characters long with hyphens and 32 characters without.")
+    assert len(customer_key) == 36, "'customer_key' must be 36 characters long with hyphens"
 
 
 def validate_security(security):
