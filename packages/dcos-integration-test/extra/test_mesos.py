bd9d5741e95e75cfe39b24c051d7583092404b7b
Adam B <adam@mesosphere.io>
2016-12-13 17:58:04 -0800

    Inline find_ helpers, pull post to top-level

--- packages/dcos-integration-test/extra/test_mesos.py
+++ packages/dcos-integration-test/extra/test_mesos.py
@@ -7,49 +7,19 @@ import requests
 from test_util.marathon import get_test_app
 from test_util.recordio import Decoder, Encoder
 
+def post(url, headers, json=None, data=None, stream=False):
+    r = requests.post(url, headers=headers, json=json, data=data, stream=stream)
+    logging.info(
+        'Got %s with POST request to %s with headers %s and json data %s.',
+        r.status_code,
+        url,
+        headers,
+        json
+    )
+    assert r.status_code == 200
+    return r
 
 def test_if_marathon_app_can_be_debugged(cluster):
-    def post(url, headers, json=None, data=None, stream=False):
-        r = requests.post(url, headers=headers, json=json, data=data, stream=stream)
-        logging.info(
-            'Got %s with POST request to %s with headers %s and json data %s.',
-            r.status_code,
-            url,
-            headers,
-            json
-        )
-        assert r.status_code == 200
-        return r
-
-    def find_container_id(state, app_id):
-        container_id = None
-        for framework in state['frameworks']:
-            for task in framework['tasks']:
-                if app_id in task['id']:
-                    container_id = task['statuses'][0]['container_status']['container_id']['value']
-        if container_id is None:
-            raise Exception('Container ID not found in task status report for instance of app_id {}'.format(app_id))
-        return container_id
-
-    def find_agent_id(state, app_id):
-        agent_id = None
-        for framework in state['frameworks']:
-            for task in framework['tasks']:
-                if app_id in task['id']:
-                    agent_id = task['slave_id']
-        if agent_id is None:
-            raise Exception('Agent ID not found for instance of app_id {}'.format(app_id))
-        return agent_id
-
-    def find_agent_hostname(state, agent_id):
-        agent_hostname = None
-        for agent in state['slaves']:
-            if agent['id'] == agent_id:
-                agent_hostname = agent['hostname']
-        if agent_hostname is None:
-            raise Exception('Agent hostname not found for agent_id {}'.format(agent_id))
-        return agent_hostname
-
     # Creates and yields the initial ATTACH_CONTAINER_INPUT message, then a data message,
     # then an empty data chunk to indicate end-of-stream.
     def _input_streamer(encoder, nested_container_id, input_data):
@@ -77,8 +47,9 @@ def test_if_marathon_app_can_be_debugged(cluster):
         yield encoder.encode(message)
 
     # Launch a basic marathon app (no image), so we can debug into it!
+    # Cannot use deploy_and_cleanup because we must attach to a running app/task/container.
     app, test_uuid = get_test_app()
-    test_app_id = 'integration-test-{}'.format(test_uuid)
+    app_id = 'integration-test-{}'.format(test_uuid)
     cluster.marathon.deploy_app(app)
 
     # Fetch the mesos master state once the task is running
@@ -89,11 +60,27 @@ def test_if_marathon_app_can_be_debugged(cluster):
     state = r.json()
 
     # Find the agent_id and container_id from master state
-    container_id = find_container_id(state, test_app_id)
-    agent_id = find_agent_id(state, test_app_id)
-    agent_hostname = find_agent_hostname(state, agent_id)
+    container_id = None
+    agent_id = None
+    for framework in state['frameworks']:
+        for task in framework['tasks']:
+            if app_id in task['id']:
+                container_id = task['statuses'][0]['container_status']['container_id']['value']
+                agent_id = task['slave_id']
+    if container_id is None:
+        raise Exception('Container ID not found for instance of app_id {}'.format(app_id))
+    if agent_id is None:
+        raise Exception('Agent ID not found for instance of app_id {}'.format(app_id))
+
+    # Find hostname and URL from agent_id
+    agent_hostname = None
+    for agent in state['slaves']:
+        if agent['id'] == agent_id:
+            agent_hostname = agent['hostname']
+    if agent_hostname is None:
+        raise Exception('Agent hostname not found for agent_id {}'.format(agent_id))
     agent_v1_url = 'http://{}:{}/api/v1'.format(agent_hostname, 5051)
-    logging.debug('Located %s with containerID %s on agent %s', test_app_id, container_id, agent_hostname)
+    logging.debug('Located %s with containerID %s on agent %s', app_id, container_id, agent_hostname)
 
     # Prepare nested container id data
     nested_container_id = {
@@ -145,4 +132,4 @@ def test_if_marathon_app_can_be_debugged(cluster):
                 assert r['data']['data'] == 'meow', 'Output did not match expected'
 
     # Destroy the app and the task's containers now that we're done with it
-    cluster.marathon.destroy_app(test_app_id)
+    cluster.marathon.destroy_app(app_id)
