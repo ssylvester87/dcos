f50fd9bc79d7994baf9da644104c90f26d16d452
Jeff Malnick <malnick@gmail.com>
2016-12-15 17:27:57 -0800

    Fix integration test for framework_principal

--- packages/dcos-integration-test/extra/test_metrics_ee.py
+++ packages/dcos-integration-test/extra/test_metrics_ee.py
@@ -14,16 +14,27 @@ def test_framework_principal_present(cluster):
     """
     def framework_principal_present(node):
         response = cluster.metrics.get('containers', node=node.host)
-        json_response = response.json()
-        if len(json_response) > 0:
-            for c in json_response:
-                container_response = cluster.metrics.get('containers/{}'.format(c), node=node.host)
-                if container_response.status_code == 200 and 'dimensions' in container_response.json():
-                    assert 'framework_principal' in json_response['dimensions'], ''
-                    '"framework_principal" not present in {}'.format(response.content)
 
-                    assert len(json_response['dimensions']['framework_principal']) != 0, ''
-                    '"framework_principal" length is 0'
+        assert response.status_code == 200, 'Excepted 200 response, got {}'.format(response.status_code)
+        json_response = response.json()
+
+        assert len(json_response) > 0, 'Expected at least 1 container present from /containers'
+        ', got {}'.format(json_response)
+
+        for c in json_response:
+            container_response = cluster.metrics.get('containers/{}'.format(c), node=node.host)
+
+            assert container_response.status_code == 200, 'Expected 200 status code, got {}, '
+            'content is {}'.format(response.status_code, response.content)
+
+            assert 'dimensions' in container_response.json(), 'Expected dimensions key in response'
+            ' got {}'.format(response.content)
+
+            assert 'framework_principal' in json_response['dimensions'], ''
+            '"framework_principal" not present in {}'.format(response.content)
+
+            assert len(json_response['dimensions']['framework_principal']) != 0, ''
+            '"framework_principal" length is 0'
 
     marathon_config = {
         # We call it '-2' in case it runs on the same host as the OSS test
