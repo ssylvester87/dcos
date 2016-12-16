13e9e6760f8b800151da65f7d73031289ea61857
Roger Ignazio <me@rogerignazio.com>
2016-12-16 10:46:15 -0800

    Fix failing metrics_ee test
    use ee_helpers

--- packages/dcos-integration-test/extra/test_metrics_ee.py
+++ packages/dcos-integration-test/extra/test_metrics_ee.py
@@ -1,44 +1,54 @@
+import uuid
+
+import ee_helpers
 import pytest
-
-from dcostests import dcos
-
-from test_util.marathon import get_test_app
 
 
 @pytest.mark.xfail(
-    dcos.config['security'] in ['disabled', 'permissive'],
+    ee_helpers.dcos_config['security'] in ['disabled', 'permissive'],
     reason='framework_principal" should only be present in strict mode',
     strict=True
 )
 def test_framework_principal_present(cluster):
-    """ Test that teh framework_principal is present as a dimension
-    in EE clusters.
+    """ Test that the framework_principal is present as a dimension in EE
+    clusters.
     """
-    def framework_principal_present(node):
-        response = cluster.metrics.get('containers', node=node.host)
+    def framework_principal_present(node, superuser):
+        """Ensure that framework_principal is present in the container's
+        dimensions struct, and that it has a non-empty value.
+        """
+        containers = cluster.metrics.get(
+            'containers',
+            node=node.host,
+            headers=superuser.auth_header)
 
-        assert response.status_code == 200, 'Excepted 200 response, got {}'.format(response.status_code)
-        json_response = response.json()
+        assert containers.status_code == 200, 'Excepted 200 response, got '
+        '{}'.format(containers.status_code)
 
-        assert len(json_response) > 0, 'Expected at least 1 container present from /containers'
-        ', got {}'.format(json_response)
+        assert len(containers.json()) > 0, 'Expected at least 1 container'
+        'present from /containers endpoint, got {}'.format(containers.json())
 
-        for c in json_response:
-            container_response = cluster.metrics.get('containers/{}'.format(c), node=node.host)
+        for container in containers.json():
+            resp = cluster.metrics.get(
+                '/'.join(['containers', container]),
+                node=node.host,
+                headers=superuser.auth_header)
 
-            assert container_response.status_code == 200, 'Expected 200 status code, got {}, '
-            'content is {}'.format(response.status_code, response.content)
+            assert resp.status_code == 200, 'Expected 200 status code, got '
+            '{}, content is {}'.format(resp.status_code, resp.content)
 
-            assert 'dimensions' in container_response.json(), 'Expected dimensions key in response'
-            ' got {}'.format(response.content)
+            assert 'dimensions' in resp.json(), 'Expected dimensions key in '
+            'response, got {}'.format(resp.content)
 
-            assert 'framework_principal' in json_response['dimensions'], ''
-            '"framework_principal" not present in {}'.format(response.content)
+            assert 'framework_principal' in resp.json()['dimensions'], '"framework_principal" '
+            'not present in {}'.format(resp.content)
 
-            assert len(json_response['dimensions']['framework_principal']) != 0, ''
-            '"framework_principal" length is 0'
+            assert resp.json()['dimensions']['framework_principal'] != "", 'Expected '
+            'framework_principal to not be empty (but it was!)'
 
-    test_app1, _ = get_test_app()
-    with cluster.marathon.deploy_and_cleanup(test_app1) as app_endpoints:
-        for node in app_endpoints:
-            framework_principal_present(node)
+        return True
+
+    app = ee_helpers.sleep_app_definition("dcos-metrics-%s" % str(uuid.uuid4()))
+    with cluster.marathon.deploy_and_cleanup(app, check_health=False) as endpoints:
+        for node in endpoints:
+            framework_principal_present(node, cluster.web_auth_default_user)
