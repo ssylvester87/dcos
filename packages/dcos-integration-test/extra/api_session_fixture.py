01ce59e0e51e200fa4a88770c8acffd4fde92557
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-01-23 16:58:55 -0800

    Refactor test_mesos_authn_authz.py

--- packages/dcos-integration-test/extra/api_session_fixture.py
+++ packages/dcos-integration-test/extra/api_session_fixture.py
@@ -7,7 +7,30 @@ from test_util.dcos_api_session import DcosApiSession, DcosUser, get_args_from_e
 from test_util.helpers import session_tempfile
 
 
-class EnterpriseApiSession(DcosApiSession):
+class MesosNodeClientMixin:
+    """ This Mixin allows any request to be made against a master or agent
+    mesos HTTP port by providing the keyword 'mesos_node'. Thus, the user
+    does not have to specify the master/agent port or which arbitrary host
+    in the cluster meeting that role
+    """
+    def api_request(self, method, path_extension, *, scheme=None, host=None, query=None,
+                    fragment=None, port=None, mesos_node=None, **kwargs):
+        if mesos_node is not None:
+            assert port is None, 'Usage error: mesos_node keyword will set port'
+            assert host is None, 'Usage error: mesos_node keyword will set host'
+            if mesos_node == 'master':
+                port = 5050
+                host = self.masters[0]
+            elif mesos_node == 'agent':
+                port = 5051
+                host = self.slaves[0]
+            else:
+                raise AssertionError('Mesos node type not recognized: {}'.format(mesos_node))
+        return super().api_request(method, path_extension, scheme=scheme, host=host, query=query,
+                                   fragment=fragment, port=port, **kwargs)
+
+
+class EnterpriseApiSession(MesosNodeClientMixin, DcosApiSession):
     @property
     def iam(self):
         new = self.copy()
