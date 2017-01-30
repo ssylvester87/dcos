4c88de01ec893a1470ceae1dd624016af97fc231
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-01-30 10:45:56 -0800

    Remove dcostests
    now covered with upstream test_util

--- packages/dcos-integration-test/extra/dcostests/__init__.py
+++
@@ -1,3 +0,0 @@
-from dcostests.dcos import dcos  # noqa
-from dcostests.utils import AuthedUser, SuperUser  # noqa
-from dcostests.url import Url, IAMUrl, CAUrl, DDDTUrl, MarathonUrl, SecretsUrl  # noqa
