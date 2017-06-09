b2b79d6553215b1be9ac2850795c5f7c8712023e
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-06-09 15:37:26 -0700

    Fix import ordering

--- packages/dcos-integration-test/extra/conftest.py
+++ packages/dcos-integration-test/extra/conftest.py
@@ -14,8 +14,8 @@ from urllib.parse import urlparse
 import pytest
 import requests
 from api_session_fixture import make_session_fixture
-from dcoscli import DCOS_CLI_URL, DCOSCLI
 from dcos_test_utils.dcos_api_session import DcosAuth, DcosUser
+from dcoscli import DCOS_CLI_URL, DCOSCLI
 from jwt.utils import base64url_decode, base64url_encode
 
 
