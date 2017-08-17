bf07e55789d9b6283cd95a19e70cd8f868ead141
Gustav Paul <gpaul@mesosphere.io>
2017-08-17 13:54:25 +0200

    test-e2e/test_restart.py: refactor test_cockroach_stop_start to check health with systemd

--- test-e2e/test_restart.py
+++ test-e2e/test_restart.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from subprocess import CalledProcessError
 
 import pytest
+import retrying
 from dcos_e2e.backends import ClusterBackend
 from dcos_e2e.cluster import Cluster
 from passlib.hash import sha512_crypt
@@ -30,7 +31,7 @@ class TestServiceStopStart:
     ):
         """
         Test that a cluster consisting of multiple master nodes becomes
-        healthy after shutting down CockroachDB on every master node
+        healthy after shutting down cockroachdb on every master node
         and starting it again.
         """
 
@@ -54,31 +55,64 @@ class TestServiceStopStart:
             # Wait for the cluster to become healthy initially.
             log.info("Waiting for cluster to become healthy.")
             cluster.wait_for_dcos()
-            # Stop CockroachDB on all master nodes.
-            stop_cmd = ['systemctl', 'stop', 'dcos-cockroach']
-            diagnostics_args = ['/opt/mesosphere/bin/dcos-diagnostics', '--diag']
-            for master in cluster.masters:
-                # Confirm that the master is healthy by running diagnostics.
-                master.run_as_root(args=diagnostics_args)
-                log.info("Stopping cockroachdb on master `{}`.".format(master.ip_address))
-                master.run_as_root(args=stop_cmd)
-                # Confirm that the master is unhealthy by asserting
-                # that diagnostics fail.
-                with pytest.raises(CalledProcessError):
-                    log.info("Confirming that diagnostics fail with stopped cockroachdb instance.")
-                    master.run_as_root(args=diagnostics_args)
 
-            # Start CockroachDB on all master nodes.
-            start_cockroach_on_masters(cluster.masters)
+            # Confirm that cockroachdb is running on all the masters.
+            _wait_for_cockroachdb_cluster_healthy(cluster.masters)
 
-            # Wait for the cluster to become healthy again.
-            log.info("Waiting for cluster to become healthy.")
-            cluster.wait_for_dcos()
+            # Stop cockroachdb on all masters.
+            _stop_cockroachdb_on_masters(cluster.masters)
+
+            # Check that cockroachdb instances are unhealthy.
+            _assert_cockroachdb_cluster_unhealthy(cluster.masters)
+
+            # Start cockroachdb on all master nodes.
+            _start_cockroachdb_on_masters(cluster.masters)
+
+            # Confirm that cockroachdb is running on all the masters.
+            _wait_for_cockroachdb_cluster_healthy(cluster.masters)
 
 
-def start_cockroach_on_masters(masters):
+@retrying.retry(
+    stop_max_delay=2*60*1000,
+    retry_on_exception=lambda x: isinstance(x, CalledProcessError))
+def _wait_for_cockroachdb_cluster_healthy(masters):
+    """Wait for cockroachdb to be running on the master."""
+    for master in masters:
+        # Confirm that the cockroachdb instance is running.
+        log.info("Confirming that cockroachdb is running on master `{}`".format(
+            master.ip_address))
+        _check_cockroachdb_status(master)
+
+
+def _assert_cockroachdb_cluster_unhealthy(masters):
+    """Wait for cockroachdb to not be running on the master."""
+    for master in masters:
+        # Confirm that the cockroachdb instance is not running.
+        log.info("Confirming that cockroachdb is not running on master `{}`".format(
+            master.ip_address))
+        with pytest.raises(CalledProcessError):
+            _check_cockroachdb_status(master)
+
+
+def _check_cockroachdb_status(master):
+    """Confirm that cockroachdb is running on the master."""
+    # Running `systemctl status` will return a non-zero exit status
+    # if the listed unit is not active.
+    cockroachdb_status_args = ['systemctl', 'status', 'dcos-cockroach']
+    master.run_as_root(args=cockroachdb_status_args)
+
+
+def _stop_cockroachdb_on_masters(masters):
+    """Stop cockroachdb on all master nodes."""
+    stop_cmd = ['systemctl', 'stop', 'dcos-cockroach']
+    for master in masters:
+        log.info("Stopping cockroachdb on master `{}`".format(master.ip_address))
+        master.run_as_root(stop_cmd)
+
+
+def _start_cockroachdb_on_masters(masters):
     """
-    CockroachDB only starts successfully once sufficient members have
+    cockroachdb only starts successfully once sufficient members have
     rejoined the cluster. That means that starting them serially won't
     work: the first will hang waiting for the others and eventually
     the test will time out.
